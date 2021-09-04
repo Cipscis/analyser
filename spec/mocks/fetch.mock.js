92ce154a42d68e84bc3a0a5a63830632ff940c02
Mark Hanna <cipscis+github@gmail.com>
2021-09-04 19:02:01 +1200

    Connect example config for tests to the fetch mock

--- spec/mocks/fetch.mock.js
+++ spec/mocks/fetch.mock.js
@@ -1,9 +1,10 @@
 import { exampleA, exampleB, exampleC } from '../data/example.data.js';
+import { exampleAConfig, exampleBConfig, exampleCConfig } from '../data/example.config.mjs';
 
 const urlMap = {
-	'city example.csv': exampleA,
-	'city example 2.csv': exampleB,
-	'city example 3.csv': exampleC,
+	[exampleAConfig.path]: exampleA,
+	[exampleBConfig.path]: exampleB,
+	[exampleCConfig.path]: exampleC,
 };
 
 const fetch = async (url) => {
