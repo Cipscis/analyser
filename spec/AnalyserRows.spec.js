fe2b09cddd0589c79f8da3f99dd23ce1842f06c1
Mark Hanna <cipscis+github@gmail.com>
2021-09-04 21:35:47 +1200

    addCol method for AnalyserRows

--- spec/AnalyserRows.spec.js
+++ spec/AnalyserRows.spec.js
@@ -7,20 +7,63 @@ import { exampleAConfig, exampleBConfig, exampleCConfig } from
 './data/example.config.js';
 
 describe(`AnalyserRows`, () => {
-	describe(`getCol`, () => {
-		it(`gets a single column`, async () => {
-			const { rows, cols } = await analyser.loadFile(exampleAConfig);
+	let rows;
+	let cols;
 
+	beforeEach(async () => {
+		const dataConfig = await analyser.loadFile(exampleAConfig);
+		rows = dataConfig.rows;
+		cols = dataConfig.cols;
+	});
+
+	describe(`getCol`, () => {
+		it(`gets a single column`, () => {
 			const names = rows.getCol(cols.NAME);
 			expect(names).toEqual(['Auckland', 'Taupō', 'Hamburg', 'Sydney', 'Hamilton', 'Wellington', 'Christchurch', 'Dunedin', 'Tauranga']);
 		});
 
-		it(`throws an error if the column does not exist`, async () => {
-			const { rows, cols } = await analyser.loadFile(exampleAConfig);
-
+		it(`throws an error if the column does not exist`, () => {
 			expect(() => rows.getCol()).toThrowError();
 			expect(() => rows.getCol(-1)).toThrowError();
 			expect(() => rows.getCol(1000)).toThrowError();
+		});
+	});
+
+	describe(`addCol`, () => {
+		it(`adds an array of the right length as a new column`, () => {
+			const pop = rows.getCol(cols.POPULATION);
+			const popTrue = pop.map((pop) => pop * 1000);
+
+			cols.POPULATION_TRUE = rows.addCol(popTrue);
+			expect(cols.POPULATION_TRUE).toBe(7);
+
+			const popTrueInRows = rows.getCol(cols.POPULATION_TRUE);
+			expect(popTrueInRows).toEqual(popTrue);
+		});
+
+		it(`throws an error if a column of the wrong length is passed`, () => {
+			const shortCol = [1, 2, 3];
+
+			expect(() => cols.SHORT = rows.addCol(shortCol)).toThrowError();
+		});
+
+		it(`accepts a function to create a new derived column based on the row`, () => {
+			const getTruePop = (row) => row[cols.POPULATION] * 1000;
+
+			cols.POPULATION_TRUE = rows.addCol(getTruePop);
+			expect(cols.POPULATION_TRUE).toBe(7);
+
+			const popTrueFromMap = rows.getCol(cols.POPULATION).map((pop) => pop * 1000);
+			const popTrueInRows = rows.getCol(cols.POPULATION_TRUE);
+			expect(popTrueInRows).toEqual(popTrueFromMap);
+
+			const getIndex = (row, i) => i;
+			cols.INDEX = rows.addCol(getIndex);
+			expect(cols.INDEX).toBe(8);
+
+			const indexCol = rows.getCol(cols.INDEX);
+			const indexFromMap = rows.map((row, i) => i);
+			expect(indexCol).toEqual(indexFromMap);
 		});
 	});
 });
--- src/AnalyserRows.ts
+++ src/AnalyserRows.ts
@@ -12,6 +12,13 @@ class AnalyserRows extends Array {
 		}
 	}
 
+	/**
+	 * Returns the specified column.
+	 *
+	 * @param  {number} colNum - The index of the column to return.
+	 *
+	 * @return {any[]} - The specified column.
+	 */
 	getCol(colNum: number): any[] {
 		if (typeof colNum !== 'number') {
 			throw new TypeError(`colNum must be a number.`);
@@ -25,6 +32,33 @@ class AnalyserRows extends Array {
 		}
 
 		return col;
+	}
+
+	/**
+	 * Adds a new column to AnalyserRows, and returns its index.
+	 *
+	 * @param  {any[] | ((row: any[], index: number) => any))} creator - Either the column to add, or a function to create the new column's values.
+	 *
+	 * @return {number} - The index of the newly added column.
+	 */
+	addCol(creator: any[] | ((row: any[], index: number) => any)): number {
+		const colIndex = this[0].length;
+
+		if (Array.isArray(creator)) {
+			if (this.length !== creator.length) {
+				throw new Error(`New column of length ${creator.length} cannot be added. It must be of length ${this.length}.`);
+			}
+
+			for (let [i, row] of this.entries()) {
+				row.push(creator[i]);
+			}
+		} else {
+			for (let [i, row] of this.entries()) {
+				row.push(creator(row, i));
+			}
+		}
+
+		return colIndex;
 	}
 }
 
