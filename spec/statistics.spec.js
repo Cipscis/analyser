f4663d134ef6ceda1e0373a09fcac51e97edb195
Mark Hanna <cipscis+github@gmail.com>
2022-01-02 16:25:36 +1300

    Allow statistics functions to take an array of numbers or any number of number arguments

--- spec/statistics.spec.js
+++ spec/statistics.spec.js
@@ -3,27 +3,27 @@ import * as statistics from '../dist/statistics.js';
 describe(`sum`, () => {
 	it(`returns the sum of an array of numbers`, () => {
 		expect(statistics.sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(55);
-		expect(statistics.sum([-1, 1, -1, 1, -1, 1])).toBe(0);
+		expect(statistics.sum(-1, 1, -1, 1, -1, 1)).toBe(0);
 	});
 });
 
 describe(`max`, () => {
 	it(`returns the largest of an array of numbers`, () => {
 		expect(statistics.max([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(10);
-		expect(statistics.max([-1, 1, -1, 1, -1, 1])).toBe(1);
+		expect(statistics.max(-1, 1, -1, 1, -1, 1)).toBe(1);
 	});
 });
 
 describe(`mean`, () => {
 	it(`returns the mean of an array of numbers`, () => {
 		expect(statistics.mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5.5);
-		expect(statistics.mean([-1, 1, -1, 1, -1, 1])).toBe(0);
+		expect(statistics.mean(-1, 1, -1, 1, -1, 1)).toBe(0);
 	});
 });
 
 describe(`min`, () => {
 	it(`returns the smallest of an array of numbers`, () => {
 		expect(statistics.min([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(1);
-		expect(statistics.min([-1, 1, -1, 1, -1, 1])).toBe(-1);
+		expect(statistics.min(-1, 1, -1, 1, -1, 1)).toBe(-1);
 	});
 });
--- src/statistics.ts
+++ src/statistics.ts
@@ -1,8 +1,18 @@
+function getNumArray(val: number[] | [number[]]): number[] {
+	if (val.length === 1 && Array.isArray(val[0])) {
+		return val[0];
+	} else {
+		return val as number[];
+	}
+}
+
 /**
  * Return the sum of an array of numbers.
  */
-function sum(arr: number[]): number {
-	return arr.reduce(function (sum: number, val: number) {
+export function sum(...arr: number[] | [number[]]): number {
+	const numbers = getNumArray(arr);
+
+	return numbers.reduce(function (sum: number, val: number) {
 		return sum + val;
 	}, 0);
 }
@@ -10,27 +20,26 @@ function sum(arr: number[]): number {
 /**
  * Returns the maximum of an array of numbers.
  */
-function max(arr: number[]): number {
-	return Math.max(...arr);
+export function max(...arr: number[] | [number[]]): number {
+	const numbers = getNumArray(arr);
+
+	return Math.max(...numbers);
 }
 
 /**
  * Returns the mean of an array of numbers.
  */
-function mean(arr: number[]): number {
-	return sum(arr) / arr.length;
+export function mean(...arr: number[] | [number[]]): number {
+	const numbers = getNumArray(arr);
+
+	return sum(numbers) / numbers.length;
 }
 
 /**
  * Returns the minimum of an array of numbers.
  */
-function min(arr: number[]): number {
-	return Math.min(...arr);
-}
+export function min(...arr: number[] | [number[]]): number {
+	const numbers = getNumArray(arr);
 
-export {
-	sum,
-	max,
-	mean,
-	min,
-};
+	return Math.min(...numbers);
+}
